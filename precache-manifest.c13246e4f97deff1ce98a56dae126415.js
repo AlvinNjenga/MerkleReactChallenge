e9120503d0e59be7a55f8eb5418d347d6ce3ac38
Alvin N <alvinnjenga@gmail.com>
2019-03-04 17:14:37 +0000

    Updates

---
+++ precache-manifest.c13246e4f97deff1ce98a56dae126415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe84d5b9962837f6daaf",
+    "url": "/MerkleReactChallenge/static/js/runtime~main.fe84d5b9.js"
+  },
+  {
+    "revision": "e655a33b47e340fd6357",
+    "url": "/MerkleReactChallenge/static/js/main.e655a33b.chunk.js"
+  },
+  {
+    "revision": "06e7f200aaaa326479ea",
+    "url": "/MerkleReactChallenge/static/js/2.06e7f200.chunk.js"
+  },
+  {
+    "revision": "e655a33b47e340fd6357",
+    "url": "/MerkleReactChallenge/static/css/main.f1d04e4e.chunk.css"
+  },
+  {
+    "revision": "5cc1e48040d43c9a8646c52d0a429369",
+    "url": "/MerkleReactChallenge/index.html"
+  }
+];
